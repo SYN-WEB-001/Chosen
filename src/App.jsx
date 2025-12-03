af4725c94709d77f2b2525166739fef5b4e0faba
Hakan Öztürk <hakanoezuerk@gmail.com>
2025-12-03 06:43:39 +0100

    compnenten navbar erstell

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Home from './pages/Home.jsx'
-import Navbar from './components/Navbar.jsx'
 
 function App() {
 
 
   return (
     <>
-      <Navbar />
       < Home />
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import HeroSection from "../components/HeroSection";
-import Login from "../components/Login.jsx";
 export default function Home() {
     return (
         <div>
             <HeroSection />
-            <Login />
         </div>
     );
 }

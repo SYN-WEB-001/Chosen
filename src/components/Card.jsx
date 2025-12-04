1ca48d6b847b17e2b747c4298c02239a7c66bca9
Zirawan salim <zirawan@hotmail.com>
2025-12-04 15:36:56 +0100

    Skip Button koregiert

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -88,7 +88,7 @@ export default function Card() {
         <div className="flex w-full mt-4 space-x-3 ">
 
           <button
-            className="btn hover:border-gradient-to-r from-pink-600 hover:to-purple-700 border-white text-white flex-1 m-2 rounded-xl"
+            className="btn hover:border-transparent hover:bg-gray-300 border-black text-black flex-1 m-2 rounded-xl "
             onClick={handleSkip}
           >
             <X className="w-5 h-5 mr-2 " />

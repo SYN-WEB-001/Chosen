f8c5eb8f6abbf3e39cfbe862f5bcd5de2776fb8e
Hakan Öztürk <hakanoezuerk@gmail.com>
2025-12-04 21:00:49 +0100

    navbar auf stick gesetz und bg beim scrollen tranparent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from "react";
 import DarkModeToggle from "../components/DarkModeToggle";
 
-
 export default function Navbar() {
-    return (
+    const [isScrolled, setIsScrolled] = useState(false);
 
-        <div className="navbar shadow-sm bg-gray-300 h-20 ">
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 10);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <div
+            className={`
+                navbar h-20 sticky top-0 z-50 transition-all duration-300
+                ${isScrolled ? "bg-transparent backdrop-blur-lg shadow-none" : "bg-gray-300 shadow-sm"}
+            `}
+        >
             <div className="flex-1">
-                <a className="text-4xl font-bold bg-gradient-to-br from-pink-500 to-purple-600 bg-clip-text text-transparent border-none ml-30">Chosen</a>
+                <a className="text-4xl font-bold bg-gradient-to-br from-pink-500 to-purple-600 bg-clip-text text-transparent border-none ml-30">
+                    Chosen
+                </a>
             </div>
+
             <div className="items-center">
                 <DarkModeToggle />
             </div>
         </div>
-
-
-    )
+    );
 }
